ac8f46385ce3c4fd10897aa3e4eaa95353b87057
Matt Mobley <mattsmobley@gmail.com>
2016-03-02 08:47:31 -0700

    fixed the main gallerygit add .

--- js/routes.js
+++ js/routes.js
@@ -2,7 +2,7 @@ angular.module("app")
 .config(function($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-      });  
+        templateUrl: 'views/main.html'
+      });
 
 });
